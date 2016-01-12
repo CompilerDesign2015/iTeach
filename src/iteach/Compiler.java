d843b12200f141ee320ea2a76278eef4bcedbb35
Ivan Wesley Chua <ivanwesleychua@yahoo.com>
2016-01-12 20:50:11 +0800

    m

--- src/iteach/Compiler.java
+++ src/iteach/Compiler.java
@@ -63,8 +63,7 @@ public class Compiler extends JPanel {
 
             }
             stack = new ArrayList<String>(lines.length);
-            checkSyntaxWhole(code.getText());
-            //parse(lines);
+            parse(lines);
         }
     }
 
@@ -82,8 +81,11 @@ public class Compiler extends JPanel {
                 + "|(container)(\\()((?:[A-Z][A-Z0-9_]*))(\\))"
                 + "|(OPEN)"
                 + "|(CLOSE)"
-                + "|(count|add)(\\()([0-5]+)(,)([0-5]+)(,)(\\w+)(\\))"
-                + "|(subtract)(\\()([0-5]+)(,)([0-5]+)(,)(\\w+)(,)(\\w+)(\\))");
+                + "|(count|add)(\\()([0-5]+)(, )([0-5]+)(, )(\\w+)(\\))"
+                + "|(subtract)(\\()([0-5]+)(, )([0-5]+)(, )(\\w+)(, )(\\w+)(\\))");
+        
+        code.getHighlighter().removeAllHighlights();
+        
         Matcher m;
         int endCnt = 0;
         int closeCnt = 0;
@@ -99,7 +101,6 @@ public class Compiler extends JPanel {
 
             m = regexBackground.matcher(line[i]);
             if (m.find()) {
-
                 endCnt++;
             }
             m = regexContainerCount.matcher(line[i]);
@@ -137,7 +138,6 @@ public class Compiler extends JPanel {
             if (i == line.length - 1 && endCnt != 0 && closeCnt != 0) {
                 JOptionPane.showMessageDialog(null, "Take a look at your closing tags!");
             }
-
         }
     }
 
@@ -148,99 +148,5 @@ public class Compiler extends JPanel {
         Editor.getContentPane().add(new Compiler());
         Editor.setVisible(true);
         Editor.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-    }
-
-    public static void checkSyntaxWhole(String code) {
-        // TODO code application logic here
-
-        String regexBackground = "(background)(\\()((?:[A-Z][A-Z0-9_]*))(\\))(\\s+)(START)(.*?)(END)";
-        String regexContainerCount = "(count|container)";
-        String regexContainer = "(container)(\\()((?:[A-Z][A-Z0-9_]*))(\\))(\\s+)(OPEN)(.*?)(CLOSE)";
-        String regexCountAddSubtract = "(count|add|subtract)";
-        String regexCountAdd = "(count|add)(\\()([0-9]+)(,)([0-9]+)(,)(\\w+)(\\))";
-        String regexSubtract = "(subtract)(\\()([0-9]+)(,)([0-9]+)(,)(\\w+)(,)(\\w+)(\\))";
-
-        // TEST CODES HERE : USE ONLY ONE "code" VARIABLE AT A TIME    
-        //Add Slide Code
-        //        String code = "background(FIELD) START container(TREE) OPEN add(1,2,APPLE) CLOSE END";
-        //Count Slide Code
-        //String code = "background(FIELD) START count(1,2,APPLE) CLOSE END";
-
-        //Subtract Slide Code
-        //        String code = "background(FIELD) START container(TREE) OPEN subtract(1,2,APPLE,BASKET) CLOSE END";
-        Pattern pBackground = Pattern.compile(regexBackground);
-        Matcher mBackground = pBackground.matcher(code);
-        int n = 1;
-        String containerCountString = null;
-        String addSubtractString = null;
-        String countString = null;
-
-        if (mBackground.find()) {
-            while (n < 9) {
-                System.out.println(mBackground.group(n++));
-                if (n == 7) {
-                    containerCountString = mBackground.group(n);
-                }
-            }
-            Pattern pContainerCount = Pattern.compile(regexContainerCount);
-            Matcher mContainerCount = pContainerCount.matcher(containerCountString);
-            if (mContainerCount.find()) {
-                System.out.println(mContainerCount.group(1));
-                if (mContainerCount.group(1).equals("count")) {
-                    Pattern pCount = Pattern.compile(regexCountAdd);
-                    Matcher mCount = pCount.matcher(containerCountString);
-                    if (mCount.find()) {
-                        n = 1;
-                        while (n < 9) {
-                            System.out.println(mCount.group(n++));
-                        }
-                    }
-                } else if (mContainerCount.group(1).equals("container")) {
-                    Pattern pContainer = Pattern.compile(regexContainer);
-                    Matcher mContainer = pContainer.matcher(containerCountString);
-                    if (mContainer.find()) {
-                        n = 1;
-                        while (n < 9) {
-                            System.out.println(mContainer.group(n++));
-                            if (n == 7) {
-                                addSubtractString = mContainer.group(n);
-                            }
-                        }
-                    }
-                    System.out.println(addSubtractString);
-                    Pattern pAddSubtract = Pattern.compile(regexCountAddSubtract);
-                    Matcher mAddSubtract = pAddSubtract.matcher(addSubtractString);
-                    if (mAddSubtract.find()) {
-                        System.out.println(mAddSubtract.group(1));
-                        if (mAddSubtract.group(1).equals("add")) {
-                            System.out.println("ADD SIYA");
-                            Pattern pAdd = Pattern.compile(regexCountAdd);
-                            Matcher mAdd = pAdd.matcher(addSubtractString);
-                            if (mAdd.find()) {
-                                n = 1;
-                                while (n < 9) {
-                                    System.out.println(mAdd.group(n++));
-                                }
-                            }
-
-                        } else if (mAddSubtract.group(1).equals("subtract")) {
-                            System.out.println("SUBTRACT SIYA");
-                            Pattern pSubtract = Pattern.compile(regexSubtract);
-                            Matcher mSubtract = pSubtract.matcher(addSubtractString);
-                            if (mSubtract.find()) {
-                                n = 1;
-                                while (n < 11) {
-                                    System.out.println(mSubtract.group(n++));
-                                }
-                            }
-                        }
-                    }
-
-                }
-            }
-
-        }
-
     }
 }
