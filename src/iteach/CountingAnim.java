001b9586fd44eedcc2cda724321d364d39980bfb
Jethro Divino <jethro.divino@gmail.com>
2016-02-08 18:58:46 +0800

    integration success

---
+++ src/iteach/CountingAnim.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package iteach;
+
+import static iteach.Counting.labelC;
+import static iteach.Subtraction.labels;
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author DANIEL KENNETH
+ */
+public class CountingAnim implements Runnable{
+    
+    Counting N;
+    public int last;
+  
+    CountingAnim(int num2) throws IOException{
+        N = new Counting();
+        last = num2;
+    }
+    
+    @Override
+    public void run(){
+        try {     
+         int i,j;
+                for ( i = 0; i < last;i++ ) {
+                   
+                    System.out.println("hehe");
+                     Image bi =ImageIO.read(new File("/Users/jethrodivino/Dropbox/Compiler Design/Templates/Templates/Counting/"+ ++i +".png"));
+                    labelC[--i].setIcon(new ImageIcon(bi.getScaledInstance(100, 100, 100)));
+                     Thread.sleep(500);
+                }
+           
+        } catch (InterruptedException ex) {
+            Logger.getLogger(SubtractionAnim.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(CountingAnim.class.getName()).log(Level.SEVERE, null, ex);
+        }
+		
+    }
+}
+
