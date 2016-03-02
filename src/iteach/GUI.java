7c8809af17d6e60bd4eb267a2c9090aefba9155a
ivanwchua <ivanwchua@gmail.com>
2016-03-02 20:23:09 +0800

    exit button code

--- src/iteach/GUI.java
+++ src/iteach/GUI.java
@@ -9,6 +9,7 @@ import javax.swing.JFrame;
 import java.awt.Button;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -124,7 +125,8 @@ public class GUI extends JFrame{
      private class ExitListener implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
-            
+            setVisible(false);
+            dispose();
         }
     }
 }
